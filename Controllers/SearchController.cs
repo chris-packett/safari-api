af248b9e8b616cc0b2b0ea3e5ac3a954f71087ee
Chris <chrispackett1998@gmail.com>
2018-09-21 11:46:30 -0400

    Built out SearchController endpt for searching by location and species

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -24,17 +24,43 @@ namespace safari_api.Controllers
             public object Results { get; set; }
         }
 
-        //GET api/search?species=<<Species>>
+        //GET api/search?species=<<Species>>&location=<<Location>>
         [HttpGet]
-        public ActionResult<ResponseObject> Get([FromQuery] string species)
+        public ActionResult<ResponseObject> Get([FromQuery] string species, string location)
         {
-            var _animal = this.db.Animals.FirstOrDefault(f => f.Species == species);
+            var _rv = new ResponseObject();
 
-            var _rv = new ResponseObject
+
+            if (location != null && species != null)
             {
-                WasSuccessful = true,
-                Results = _animal
-            };
+                var _animals = this.db.Animals.Where(f => f.LocationOfLastSeen == location);
+                var _animalFromSpecificLocation = _animals.FirstOrDefault(f => f.Species == species);
+
+                _rv.WasSuccessful = true;
+                _rv.Results = _animalFromSpecificLocation;
+            }
+            else if (location != null || species != null)
+            {
+                if (location != null)
+                {
+                    var _animals = this.db.Animals.Where(f => f.LocationOfLastSeen == location);
+                    _rv.WasSuccessful = true;
+                    _rv.Results = _animals;
+                }
+                if (species != null)
+                {
+                    var _animal = this.db.Animals.FirstOrDefault(f => f.Species == species);
+                    _rv.WasSuccessful = true;
+                    _rv.Results = _animal;
+                }
+            }
+            else
+            {
+                _rv.WasSuccessful = false;
+                _rv.Results = new {
+                    message = "Please make sure you use at least one URL paramater."
+                };
+            }
 
             return _rv;
         }
